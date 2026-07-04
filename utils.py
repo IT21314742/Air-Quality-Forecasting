561c986d2046a05d95bc4627dfd2f99c093019f5
IT21314742 <it21314742@my.sliit.lk>
2026-07-04 17:59:31 +0530

    adding libs
    adding libs

--- utils.py
+++ utils.py
@@ -7,4 +7,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics
+from sklearn.metrics import (
+    mean_absolute_error,
+    
+)
