b48237e6e2eb3e531dd3afd7c216247aef99bc4d
IT21314742 <it21314742@my.sliit.lk>
2026-07-06 17:32:32 +0530

    Rolling mean features
    Rolling mean features

--- utils.py
+++ utils.py
@@ -69,4 +69,5 @@ def create_features(df):
     df["lag_2"] = df["pm2.5"].shift(24)     # same hour, 1 day ago
     
     # Rolling mean features - window ends 1 period before the current row
-    df["roll_24"] = df["pm2.5"]
+    df["roll_24"] = df["pm2.5"].shift(1).rolling(window=24).mean()
+    
