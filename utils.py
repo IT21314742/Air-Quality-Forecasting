cdaeb2b548e21118bc61211c8fe0ab8359d65dbd
IT21314742 <it21314742@my.sliit.lk>
2026-07-07 21:59:46 +0530

    grsph config
    grsph config

--- utils.py
+++ utils.py
@@ -112,3 +112,4 @@ def plot_predictions(y_true, y_pred, name, n=500):
     fig, ax = plt.subplots(figsize=(14, 4))
     ax.plot(np.array(y_true)[:n], label="Actual", color="steelblue", linewidth=1.2)
     ax.plot(np.array(y_pred[:n]))
+    
