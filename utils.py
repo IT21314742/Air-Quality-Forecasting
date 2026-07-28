c7cc5875d59c5406c6b27006ece3cdb52d03c423
IT21314742 <it21314742@my.sliit.lk>
2026-07-28 16:49:45 +0530

    config cross_validate_model
    config cross_validate_model

--- utils.py
+++ utils.py
@@ -135,7 +135,8 @@ def plot_residuals(y_true, y_pred, name):
 
 def cross_validate_model(model, X, y, cv=5):
     """
-    Time-series aware cross-validation using sklearn's TimeSeriesPlit."""
+    Time-series aware cross-validation using sklearn's TimeSeriesSplit."""
+    
     
     
 
